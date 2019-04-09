e99df08cbfa20a57e825e0def042bef64e57d057
kom3 <moer@student.uns.ac.id>
2019-04-09 23:45:08 +0700

    Menambahkan validation

--- HTML/Tugas/moerblog/js/script.js
+++ HTML/Tugas/moerblog/js/script.js
@@ -62,15 +62,25 @@ $(document).ready(function() {
         let email = $("#email").val();
         var komentar = $("#komentar")[0].value;
 
-        var d = new Date();
-        var o = {year:'numeric', month:'2-digit', day:'2-digit', hour:'2-digit', minute:'2-digit', second:'2-digit'};
+        var passed = true;
 
-        var e = d.toLocaleDateString('en-US', o);
+        if (passed === true && name.length < 5) passed = 'Masukkan nama minimal 5 karakter!';
+        if (passed === true && komentar.length < 20) passed = 'Masukkan minimal 20 karakter untuk komentar!';
+        if (passed === true && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) == false) passed = 'Masukkan email yang valid!';
 
-        $(".comment-list").append('<div class="comment-item"><div class="comment-box"><div class="comment-content"><div class="comment-author"><span style="font-weight: bold;">'+name+'</span><div class="time"><span class="help-text">'+email+' - '+e+'</span></div></div><div class="comment-body"><p>'+komentar+'</p><a href="#" class="button right"><i class="fas fa-reply"></i> Balas</a></div></div></div></div>');
-        
-        $("#name").val('');
-        $("#email").val('')
-        $("#komentar").val('');
+        if (passed === true) {
+            var d = new Date();
+            var o = {year:'numeric', month:'2-digit', day:'2-digit', hour:'2-digit', minute:'2-digit', second:'2-digit'};
+
+            var e = d.toLocaleDateString('en-US', o);
+
+            $(".comment-list").append('<div class="comment-item"><div class="comment-box"><div class="comment-content"><div class="comment-author"><span style="font-weight: bold;">'+name+'</span><div class="time"><span class="help-text">'+email+' - '+e+'</span></div></div><div class="comment-body"><p>'+komentar+'</p><a href="#" class="button right"><i class="fas fa-reply"></i> Balas</a></div></div></div></div>');
+            
+            $("#name").val('');
+            $("#email").val('')
+            $("#komentar").val('');
+        } else {
+            alert(passed); return false;
+        }
     })
 })
